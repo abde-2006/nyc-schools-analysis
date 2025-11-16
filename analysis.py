1e5041c6a086ff34637898fc8db1c68aab64c1f5
abdessamad <kali.amarir2006@gmail.com>
2025-11-16 08:08:50 +0100

    Add find_top_schools function with n parametre equal 10

--- analysis.py
+++ analysis.py
@@ -19,5 +19,22 @@ def load_data():
     print("\nColumns Names:")
     print(df.columns.tolist())
 
+def find_top_schools(n=10):
+    df['total_sat'] = df['average_math'] + df['average_reading'] + df['average_writing']
+
+    sort_df = df.sort_values('total_sat', ascending=False).reset_index(drop=True)
+
+    columns = ['school_name', 'borough', 'average_math', 'average_reading', 'average_writing', 'total_sat']
+    top_n = sort_df.head(n)[columns]
+    top_n.index = top_n.index + 1
+
+    print(f"\nTop {n} Schools by SAT Score:")
+    print("="*127)
+    print(top_n)
+    print("="*127)
+
+    return top_n
+
 if __name__ == "__main__":
     load_data()
+    find_top_schools()
